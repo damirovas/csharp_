6c38f7e562abc21c2ea3d36b58b80e928ca000be
damirovas <sdamirova2004@gmail.com>
2024-03-22 00:17:19 +0400

    ...........

---
+++ loops/ConsoleApp1/Program.cs
@@ -0,0 +1,71 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //int num = 5;
+            //while (num < 100)
+            //{
+            //    num += 5;
+            //    Console.WriteLine(num);
+            //}
+
+            //int result = 0;
+
+            //for(int i = 0;i<=10; i++)
+            //{
+            //    if (i % 2 == 0)
+            //    {
+            //        result += i;
+
+            //    }
+
+            //}
+            //Console.WriteLine(result);
+
+            //int result = 1;
+            //for(int i = 1;i<=10;i++)
+            //{
+            //    if(i%2==1)
+            //    {
+            //        result *= i;
+            //    }
+            //}
+            //Console.WriteLine(result);
+
+            //int result = 0;
+
+            //for (int i = 0;i<10; i++)
+            //{
+            //    if(i!=5 && i != 7)
+            //    {
+            //        result += i;
+            //    }
+            //}
+            //Console.WriteLine(result);
+
+            //int  num = int.Parse(Console.ReadLine());
+            //int result = 0;
+            //for(int i = 0;i<=num; i++)
+            //{
+            //    result += i;
+            //    if (result > 40) break;
+
+            //}
+            //Console.WriteLine(result);
+
+            //Console.WriteLine(" edeed daxil edin");
+            //int num = int.Parse(Console.ReadLine());
+
+
+            //int result = 0;
+            //while(num>0)
+            //{
+            //    result += num % 10;
+            //    num /= 10;
+            //}
+            //Console.WriteLine(result);
+        }
+    }
+}
